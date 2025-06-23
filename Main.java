bed6339dbb5c8eb656a12ea5ecf4e4e7f89d69cd
ElianCardona <elian6cardona@gmail.com>
2025-06-23 09:31:29 -0600

    Mostrandole a chico Villeda

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         Persona persona1 = new Persona();
         persona1.setNombre("Elian");
+        System.out.println("Hola");
         System.out.println(persona1.getNombre());
     }
 }
