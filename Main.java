19147dd966efa0ea7ec3917bea9bbfb0f2689db9
ElianCardona <elian6cardona@gmail.com>
2025-06-22 23:30:30 -0600

    Arregle la clase persona y cree main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Persona persona1 = new Persona();
+        persona1.setNombre("Elian");
+        System.out.println(persona1.getNombre());
+    }
+}
--- Persona.java
+++ Persona.java
@@ -1,6 +1,31 @@
 public class Persona {
+   private String nombre;
+   private String apellido;
+   private int edad;
 
-    public static void main(String[] args){
-        System.out.println("HelloWorld");
-    }
+
+   public String getNombre(){
+       return nombre;
+   }
+   
+   public String getApellido(){
+       return apellido;
+   }
+
+    public int getEdad(){
+       return edad;
+   }
+
+   public void setNombre(String nombre){
+       this.nombre = nombre;
+   }
+
+   public void setApellido(String apellido){
+       this.apellido = apellido;
+   }
+
+   public void setEdad(int edad){
+       this.edad = edad;
+   }
+
 }
